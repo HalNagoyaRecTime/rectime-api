b99790c26e69392e6c0ddac3d2b8d66183eb8c86
Kawahara <My.favorite.is.energy@gmail.com>
2026-06-01 17:35:34 +0900

    d1: d1のマイグレーション用にseed.tsからマイグレーション用のSQLを推測して作成

---
+++ migrations/0001_initial_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS m_students (
+  f_student_id  INTEGER PRIMARY KEY AUTOINCREMENT,
+  f_student_num TEXT    NOT NULL UNIQUE,
+  f_class       TEXT    NOT NULL,
+  f_number      TEXT    NOT NULL,
+  f_name        TEXT    NOT NULL,
+  f_note        TEXT
+);
+
+CREATE TABLE IF NOT EXISTS t_events (
+  f_event_id    INTEGER PRIMARY KEY AUTOINCREMENT,
+  f_event_code  TEXT    NOT NULL UNIQUE,
+  f_event_name  TEXT    NOT NULL,
+  f_time        TEXT    NOT NULL,
+  f_duration    TEXT    NOT NULL,
+  f_place       TEXT    NOT NULL,
+  f_gather_time TEXT    NOT NULL,
+  f_summary     TEXT
+);
+
+CREATE TABLE IF NOT EXISTS t_entries (
+  f_entry_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+  f_student_id INTEGER NOT NULL REFERENCES m_students(f_student_id),
+  f_event_id   INTEGER NOT NULL REFERENCES t_events(f_event_id),
+  UNIQUE (f_student_id, f_event_id)
+);
