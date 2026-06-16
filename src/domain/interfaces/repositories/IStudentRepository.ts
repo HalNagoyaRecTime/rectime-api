ea213530b2d0bf5560c324bc3ae38369dca04c45
yamada-michel27 <f.atsuya.personal@gmail.com>
2026-06-16 12:54:15 +0900

    domain/interfaces/repositories にリポジトリインターフェースを追加

---
+++ src/domain/interfaces/repositories/IStudentRepository.ts
@@ -0,0 +1,7 @@
+import { StudentEntity } from '../../entities/Student';
+
+export interface IStudentRepository {
+  findById: (id: number) => Promise<StudentEntity | null>;
+  findAll: () => Promise<StudentEntity[]>;
+  findByStudentNum: (studentNum: string) => Promise<StudentEntity | null>;
+}
