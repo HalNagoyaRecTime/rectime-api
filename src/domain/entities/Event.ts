bcfbc262fd57c9ebf56b4a0abec1178015299667
藤浦晃士 <fuji.160805@gmail.com>
2026-06-30 23:31:33 +0900

    domain層のEvent情報のインターフェイスを作成した

--- src/domain/entities/Event.ts
+++ src/domain/entities/Event.ts
@@ -8,3 +8,13 @@ export interface EventEntity {
   f_gather_time: string;
   f_summary: string | null;
 }
+//Event情報
+export interface EventInformationEntity {
+  f_event_id: number;
+  f_event_name: string;
+  f_gather_time: string;
+  f_place: string;
+  f_time: string; // 「0930」などHHMM文字列
+  f_duration: string; // 「20」等分単位文字列
+  f_summary: string | null;
+}
