7c1b21ac7b4ebe5320e1d4b1fcdf301cd33bd7d1
takarea <157473679+takarea@users.noreply.github.com>
2026-07-17 22:58:31 +0900

    change:ファイル名および、変数名など変更

---
+++ src/application/services/ClassRoomService.ts
@@ -0,0 +1,18 @@
+import { ClassRoomDTO } from '../dto/ClassRoomDTO';
+import { IClassRoomRepository } from '../../domain/interfaces/repositories/IClassRoomRepository';
+import { IClassRoomService } from './IClassRoomService';
+
+export function createClassRoomService(
+  classRepository: IClassRoomRepository
+): IClassRoomService {
+  return {
+    async getAllClassRooms(): Promise<ClassRoomDTO[]> {
+      const classRooms = await classRepository.findAll();
+      return classRooms.map(classroom => ({
+        class_room_id: classroom.f_class_room_id,
+        class_code: classroom.f_class_code,
+        class_name: classroom.f_class_name,
+      }));
+    },
+  };
+}
