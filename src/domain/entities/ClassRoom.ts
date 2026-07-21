0e282e22703067a700e55913234aa1d5bbbff658
takarea <157473679+takarea@users.noreply.github.com>
2026-07-21 14:32:25 +0900

    fix:prefix削除

--- src/domain/entities/ClassRoom.ts
+++ src/domain/entities/ClassRoom.ts
@@ -1,5 +1,5 @@
 export interface ClassRoomEntity {
-  f_class_room_id: number;
-  f_class_code: string;
-  f_class_name: string;
+  class_room_id: number;
+  class_code: string;
+  class_name: string;
 }
