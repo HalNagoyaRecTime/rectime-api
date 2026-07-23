fd1424f01d2175b4a9f4b1b0cd79800f99d6c8e0
藤浦晃士 <fuji.160805@gmail.com>
2026-07-23 09:01:27 +0900

    コンフリクトの解消

--- src/domain/entities/Event.ts
+++ src/domain/entities/Event.ts
@@ -10,11 +10,12 @@ export interface EventEntity {
 }
 //Event情報
 export interface EventInformationEntity {
-  f_event_id: number;
-  f_event_name: string;
-  f_gather_time: string;
-  f_place: string;
-  f_time: string; // 「0930」などHHMM
-  f_duration: string; // 「20」等分単位文字列
-  f_summary: string | null;
+  event_id: number;
+  event_name: string;
+  rule_text: string | null;
+  venue: string;
+  start_time: string; // JSTのHHMM形式（例: "0930"）
+  end_time: string; // JSTのHHMM形式（例: "1745"）
+  created_at: string;
+  updated_at: string;
 }
