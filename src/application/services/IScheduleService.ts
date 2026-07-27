153e84977925306775ceba1036100efebbfc04e9
takarea <157473679+takarea@users.noreply.github.com>
2026-07-28 02:23:35 +0900

    add:create,delete,history

--- src/application/services/IScheduleService.ts
+++ src/application/services/IScheduleService.ts
@@ -1,6 +1,13 @@
-import type { ScheduleDTO } from '../dto/ScheduleDTO';
+import {
+  ScheduleDTO,
+  ScheduleWriteDTO,
+  ScheduleHistoryDTO,
+} from '../dto/ScheduleDTO';
 
 export interface IScheduleService {
   getAllSchedules: () => Promise<ScheduleDTO>;
   getScheduleById: (id: number) => Promise<ScheduleDTO | null>;
+  createSchedule: (schedule: ScheduleWriteDTO) => Promise<ScheduleDTO>;
+  deleteSchedule: (id: number) => Promise<void>;
+  getHistorySchedules: (user_id: number) => Promise<ScheduleHistoryDTO | null>;
 }
