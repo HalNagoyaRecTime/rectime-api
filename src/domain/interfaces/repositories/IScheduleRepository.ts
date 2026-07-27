0a6453f07ec76a0cc4df9638618858d1c7b5b840
takarea <157473679+takarea@users.noreply.github.com>
2026-07-28 02:23:48 +0900

    add:delete,history

--- src/domain/interfaces/repositories/IScheduleRepository.ts
+++ src/domain/interfaces/repositories/IScheduleRepository.ts
@@ -1,6 +1,8 @@
-import { ScheduleEntity } from '../../entities/Schedule';
+import { ScheduleEntity, ScheduleHistoryEntity } from '../../entities/Schedule';
 
 export interface IScheduleRepository {
   findAll: () => Promise<ScheduleEntity>;
   findById: (id: number) => Promise<ScheduleEntity | null>;
+  deleteById: (id: number) => Promise<void>;
+  findByUserId: (user_id: number) => Promise<ScheduleHistoryEntity | null>;
 }
