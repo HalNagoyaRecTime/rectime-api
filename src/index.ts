e48772d7aba8d2e510d53d058bfd7f264d570c95
takarea <157473679+takarea@users.noreply.github.com>
2026-07-28 23:54:21 +0900

    delete:history

--- src/index.ts
+++ src/index.ts
@@ -227,9 +227,6 @@ apiV1.get('/notification/schedules', c => {
 apiV1.get('/notification/schedules/:scheduleId', c => {
   return c.get('container').scheduleController.getScheduleById(c);
 });
-apiV1.get('/notification/schedules/history/:user_id', c => {
-  return c.get('container').scheduleController.getHistorySchedules(c);
-});
 
 // Notification routes
 apiV1.post('/notifications', c => {
