7acc3d16748723cafcd367ecb46a542e78039a9e
takarea <157473679+takarea@users.noreply.github.com>
2026-07-30 23:56:40 +0900

    add:update用のものを追加

--- src/application/dto/ScheduleDTO.ts
+++ src/application/dto/ScheduleDTO.ts
@@ -38,5 +38,13 @@ export interface ScheduleWriteDTO {
   notification_id: number;
   event_id: number;
   importance: number;
-  send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00.000Z
+  send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00Z
+}
+
+export interface ScheduleUpdateDTO {
+  user_id: number;
+  event_id: number;
+  importance: number;
+  send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00Z
+  gathering_id: number;
 }
--- src/domain/entities/Schedule.ts
+++ src/domain/entities/Schedule.ts
@@ -41,3 +41,22 @@ export interface ScheduleWriteEntity {
   importance: number;
   send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00.000Z
 }
+
+export interface ScheduleUpdateEntity {
+  user_id: number;
+  event_id: number;
+  firebase_token_id?: number;
+  importance: number;
+  send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00.000Z
+  gathering_id: number;
+}
+
+export class HttpError extends Error {
+  constructor(
+    public statusCode: 400 | 404 | 409 | 500,
+    message: string
+  ) {
+    super(message);
+    this.name = 'HttpError';
+  }
+}
