16e2d33e0607665748512751dafc9aca6cd60499
takarea <157473679+takarea@users.noreply.github.com>
2026-08-01 01:54:27 +0900

    delete:errorのclassを削除

--- src/domain/entities/Schedule.ts
+++ src/domain/entities/Schedule.ts
@@ -1,18 +1,8 @@
 export interface ScheduleUpdateEntity {
-  user_id: number;
-  event_id: number;
+  create_user_id: number;
+  new_event_id: number;
   firebase_token_id?: number;
-  importance: number;
-  send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00.000Z
-  gathering_id: number;
-}
-
-export class HttpError extends Error {
-  constructor(
-    public statusCode: 400 | 404 | 409 | 500,
-    message: string
-  ) {
-    super(message);
-    this.name = 'HttpError';
-  }
+  new_importance: number;
+  new_send_at: string; // ISO 8601形式（UTCオフセットを含む）。例: 2026-07-16T09:00:00.000Z
+  new_gathering_id: number;
 }
