82cc02ae640ce10323347d5c4ab5b2539022297f
takarea <157473679+takarea@users.noreply.github.com>
2026-09-16 12:14:36 +0900

    delete:ハンドラがないため削除

--- src/presentation/openapi/events.ts
+++ src/presentation/openapi/events.ts
@@ -235,7 +235,6 @@ export const eventUpdateRoute = createRoute({
     401: unauthorizedResponse,
     403: forbiddenResponse,
     404: notFoundResponse,
-    409: conflictResponse,
     500: internalServerErrorResponse,
   },
 });
