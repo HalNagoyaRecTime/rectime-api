355ade8049195d86bc7019a732364363b9d11034
Wataru Miyamoto <watarumei0111@gmail.com>
2026-09-17 12:01:30 +0900

    refactor: remove unused gathering list dto

--- src/presentation/openapi/gatherings.ts
+++ src/presentation/openapi/gatherings.ts
@@ -89,14 +89,6 @@ export const gatheringResponseSchema = z
 
 export type GatheringResponseDTO = z.infer<typeof gatheringResponseSchema>;
 
-export const gatheringListResponseSchema = z
-  .array(gatheringResponseSchema)
-  .openapi('GatheringList');
-
-export type GatheringListResponseDTO = z.infer<
-  typeof gatheringListResponseSchema
->;
-
 export const gatheringSpotIdParams = z.object({
   gatheringSpotId: positivePathParam('gatheringSpotId', '集合場所ID'),
 });
